503d8518b687d3886f5241a8a5897a45f93dfc8b
Rochelle Schofield <CodeGirl@She-Codes-Mac.attlocal.net>
2018-10-11 23:54:11 -0400

    Added Folder with Mentors Info

--- client/src/pages/Mentors.js
+++ client/src/pages/Mentors.js
@@ -1,66 +1,156 @@
 import React, { Component } from "react";
-import API from "../utils/API";
+// import API from "../utils/API";
 import Card from "../components/Card";
-import Alert from "../components/Alert";
+// import Alert from "../components/Alert";
 import Container from "../components/Container";
+import Row from "../components/Row";
 
 class Mentor extends Component {
   state = {
-    image: "",
-    match: false,
-    matchCount: 0
-  };
-
-  // When the component mounts, load the next dog to be displayed
-  componentDidMount() {
-    this.loadNextDog();
-  }
-
-  handleBtnClick = event => {
-    // Get the data-value of the clicked button
-    const btnType = event.target.attributes.getNamedItem("data-value").value;
-    // Clone this.state to the newState object
-    // We'll modify this object and use it to set our component's state
-    const newState = { ...this.state };
-
-    if (btnType === "pick") {
-      // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-      newState.match = 1 === Math.floor(Math.random() * 5) + 1;
-
-      // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-      newState.matchCount = newState.match
-        ? newState.matchCount + 1
-        : newState.matchCount;
-    } else {
-      // If we thumbs down'ed the dog, we haven't matched with it
-      newState.match = false;
-    }
-    // Replace our component's state with newState, load the next dog image
-    this.setState(newState);
-    this.loadNextDog();
-  };
-
-  loadNextDog = () => {
-    API.getRandomDog()
-      .then(res =>
-        this.setState({
-          image: res.data.message
-        })
-      )
-      .catch(err => console.log(err));
+    clickCard: ""
   };
 
   render() {
     return (
-      <div>
-        <h1 className="text-center">Choose A Mentor</h1>
+      <Container>
+        <div>
+          <h1 className="text-center">Choose A Mentor</h1>
+          <Row>
+            <div className="card-deck">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src=".../100px200/"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
 
-        <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
-        <h1 className="text-center">{this.state.matchCount}!</h1>
-        <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">
-          !!!
-        </Alert>
-      </div>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src=".../100px200/"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src=".../100px200/"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Row>
+          <Row>
+            <div className="card-deck">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src=".../100px200/"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src=".../100px200/"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src=".../100px200/"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Row>
+
+          {/* <Card image={this.state.clickCard} handleBtnClick={this.handleBtnClick} /> */}
+        </div>
+      </Container>
     );
   }
 }
