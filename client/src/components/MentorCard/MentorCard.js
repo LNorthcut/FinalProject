5bc90e9efa76915cf1cbe40422e6d6f0dc1a4ebd
cookerlooker <stillettocode@gmail.com>
2018-10-18 19:57:16 -0400

    added SingleMentorPage component

--- client/src/components/MentorCard/MentorCard.js
+++ client/src/components/MentorCard/MentorCard.js
@@ -3,8 +3,13 @@ import "./MentorCard.css";
 
 const MentorCard = props => (
   <div className="card">
+    {/* <a href="http://google.com">
+      link */}
     <div className="img-container">
       <img alt={props.name} src={props.image} />
+      <button type="button" class="btn btn-success">
+        Check Me Out
+      </button>
     </div>
     <div className="content">
       <ul>
@@ -34,7 +39,24 @@ const MentorCard = props => (
     {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
       𝘅
     </span> */}
+    {/* </a> */}
   </div>
 );
 
 export default MentorCard;
+
+/*
+In your Mentors.js page file, You need to add a nested route
+
+So something like
+
+<Switch>
+    <Route path="/mentors/:id" component={SingleMentorPage} />
+</Switch>
+
+Then you need to create the SingleMentorPage component to render a single mentor
+
+Then you need to pass the mentor's ID as a prop to MentorCard from the Mentors.js page
+
+and change the "name: {props.name}" to "name: <Link to={`/mentors/${props.id}`}>{props.name}</Link>"
+*/
--- client/src/pages/Mentors.js
+++ client/src/pages/Mentors.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import MentorCard from "../components/MentorCard";
 import Wrapper from "../components/Wrapper";
 import Title from "../components/Title";
+import "./MentorsPage.css";
 
 import mentors from "./mentors.json";
 // import "./MentorCard.css";
@@ -11,12 +12,19 @@ class Mentor extends Component {
     mentors
   };
 
+  removeMentor = id => {
+    // Filter this.state.mentors for mentors with an id not equal to the id being removed
+    const mentors = this.state.mentors.filter(mentor => mentor.id !== id);
+    // Set this.state.mentors equal to the new mentors array
+    this.setState({ mentors });
+  };
+
   // Map over this.state.mentors and render a MentorCard component for each mentor object
 
   render() {
     console.log("Props: ", this.props);
     return (
-      <Wrapper>
+      <Wrapper className="wrapper-main">
         <Title>Our Mentors </Title>
 
         {this.state.mentors.map(mentor => (
